e7911e21b6979a528a5b90d98b1e1de6d77c7097
Vlad <stankevichvlad338@gmail.com>
2023-03-28 19:52:49 +0300

    first commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Владислав Станкевич on 28.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
